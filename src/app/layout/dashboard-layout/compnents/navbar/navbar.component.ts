98abe5d8ebdf2861c53bf4cc49cb4889a00d558d
sayed <sayed.khalil@beetelware.net>
2025-04-28 21:15:55 +0300

    add and finish pages

--- src/app/layout/dashboard-layout/compnents/navbar/navbar.component.ts
+++ src/app/layout/dashboard-layout/compnents/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component ,HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -8,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  classToggled = false;
+  isScrolled = false;
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+    this.isScrolled = scrollTop > 50; // يعني لو سكرول أكتر من 50 بيكسل ضيف الكلاس
+  }
+  public toggleField() {
+    this.classToggled = !this.classToggled;
+  }
 
 }
